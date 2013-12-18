f4793da748b24107b6eec91aff9962a02e9b21a2
stack72 <public@paulstack.co.uk>
2013-12-18 16:48:25 +0000

    Changed the structure to now be a singlenode so we can add multinode easily enough

---
+++ setup/single-bootstrap.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+wget http://ha.geteventstore.com/showcase/EventStore-Mono-v2.5.0rc4.tar.gz
+
+tar -xvzf EventStore-Mono-v2.5.0rc4.tar.gz
+
+
+if [ ! -f "EventStore-Mono-v2.5.0rc4/singlenode-config.json" ]; then
+  echo "file missing"
+  cat <<EOF > EventStore-Mono-v2.5.0rc4/singlenode-config.json
+{ ip: "192.168.50.11"}
+EOF
+fi
+
+cd EventStore-Mono-v2.5.0rc4/
+
+./singlenode&
